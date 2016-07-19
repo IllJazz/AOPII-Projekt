c83041f418227eeb1bb7bad85abc8bb5877dcb0f
Markus Müller <markusmueller97@hotmail.de>
2016-07-19 16:48:27 +0200

    main

--- src/lgs/Main.java
+++ src/lgs/Main.java
@@ -15,30 +15,31 @@ public class Main {
 		testGUI.setVisible(true);
 		
 //		Anlegen eines Arrays der Groeße i x j
-//		zuf�llige Belegung mit Werten zwischen 0 und 100
+//		zufällige Belegung mit Werten zwischen 0 und 100
 //		i, j >= 1
-		double[][] array = new double[5][6];
-		Random random = new Random();
-		for(int i = 0; i < array.length; i++) {
-			for(int j = 0; j < array[0].length; j++){
-				array[i][j] = (int)(random.nextDouble() * 100);
-			}
-		}
+//		double[][] array = new double[5][6];
+//		Random random = new Random();
+//		for(int i = 0; i < array.length; i++) {
+//			for(int j = 0; j < array[0].length; j++){
+//				array[i][j] = (int)(random.nextDouble() * 100);
+//			}
+//		}
+		
 //		Matrix anlegen und Array einlesen
 //		Konsolenausgabe der Matrix
 //		Lösen der Matrix
-		try {
+//		try {
 //			Matrix matrix = new Matrix(array);
-			FileIO neueMatrix = new FileIO(); 					// Test FileInput
-			Matrix matrix = new Matrix(neueMatrix.getMatrix());	// Test FileInput
-			System.out.println(matrix);
-			double[] solution = new double[array.length]; 
-			solution = matrix.solve();
-			System.out.println("L�sung:\n" + Matrix.toString(solution));
-			neueMatrix.writeFile(matrix);						// Test FileOutput
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+//			FileIO neueMatrix = new FileIO(); 					// Test FileInput
+//			Matrix matrix = new Matrix(neueMatrix.getMatrix());	// Test FileInput
+//			System.out.println(matrix);
+//			double[] solution = new double[array.length]; 
+//			solution = matrix.solve();
+//			System.out.println("L�sung:\n" + Matrix.toString(solution));
+//			neueMatrix.writeFile(matrix);						// Test FileOutput
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
