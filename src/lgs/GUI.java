e930c5ae7054126358d6a4045b0c9a0db19a8a38
RxTyle <rick.werner.zittau@googlemail.com>
2016-07-20 21:18:40 +0200

    GUI-Anpassung

--- src/lgs/GUI.java
+++ src/lgs/GUI.java
@@ -19,22 +19,28 @@ public class GUI extends JFrame implements ActionListener{
 	
 	JPanel panel;
 	
-	
 	//Menue Leiste
 	JMenuBar menuBar;
 	//Menue Datei
 	JMenu fileMenu;
-	//Menuepunkt oeffnen, schlieﬂen
+	//Menue Hilfe
+	JMenu helpMenu;
+	//Menuepunkt oeffnen, schlie√üen
 	JMenuItem openItem;
+	JMenuItem saveItem;
 	JMenuItem closeItem;
+	JMenuItem helpItem;
 	
 	JButton calcButton;
 	JButton closeButton;
 	
 	JTextArea enterMatrix;
 	JTextArea outMatrix;
+	JTextArea outResiduum;
+	JTextField oute;
+	JTextField outa;
 	
-	//ermoeglicht, dass die Groeﬂe erkannt wird, die gebraucht wird
+	//ermoeglicht, dass die Groe√üe erkannt wird, die gebraucht wird
 	Dimension size;
 
 	
@@ -46,7 +52,6 @@ public class GUI extends JFrame implements ActionListener{
 		panel.setLayout(null);
 		
 
-		
 		label = new JLabel("Geben Sie hier Ihre Matrix ein:");
 		size=label.getPreferredSize();
 		label.setBounds(20, 10, size.width, size.height);
@@ -56,19 +61,28 @@ public class GUI extends JFrame implements ActionListener{
 		menuBar = new JMenuBar();
 		//Menue Datei wird erzeugt
 		fileMenu = new JMenu("Datei");
+		//Menue Hilfe wird erzeugt
+		helpMenu = new JMenu("Hilfe");
 		//Menuepunkt oeffnen
-		openItem = new JMenuItem("÷ffnen");
+		openItem = new JMenuItem("√ñffnen");
+		saveItem = new JMenuItem("Speichern");
 		closeItem = new JMenuItem("Beenden");
+		helpItem = new JMenuItem("Hilfe");
 		
 		//Menuepunkte werden dem Datei-Menue hinzugefuegt
 		fileMenu.add(openItem);
+		fileMenu.add(saveItem);
 		fileMenu.add(closeItem);
+		
+		helpMenu.add(helpItem);
 		//Datei Menue wird der Menueleiste hinzugefuegt
 		menuBar.add(fileMenu);
+		menuBar.add(helpMenu);
 		//Menueleiste wird dem JFrame hinzugefuegt
 		this.add(menuBar, BorderLayout.NORTH);
 		//ActionListener
 		openItem.addActionListener(this);
+		saveItem.addActionListener(this);
 		closeItem.addActionListener(this);
 		
 		//Matrix Eingabefeld erstellen
@@ -104,18 +118,46 @@ public class GUI extends JFrame implements ActionListener{
 		
 		if(e.getSource()==this.calcButton){
 			JOptionPane.showMessageDialog(null, "Hier muss mit Ilja zum einlesen verknuepft werden und Markus das Ergebnis ausgeben");
-			//Hier kann direkt mit der Loesungsausgabe begonnen werden
-			outMatrix = new JTextArea("Hier hinein kommt Deine Ausgabe als String, Markus");
-			outMatrix.setBounds(410, 30, 350, 400);
-			outMatrix.setVisible(true);
-			this.panel.add(outMatrix);
 			
 			resultLabel = new JLabel();
-			resultLabel.setText("Das Ergebnis ist:");
+			resultLabel.setText("Die Matrix hat + hier String von Markus + L√∂sung");
 			size = resultLabel.getPreferredSize();
 			resultLabel.setBounds(410, 10, size.width, size.height);
 			this.panel.add(resultLabel);
 			
+			//Hier kann direkt mit der Loesungsausgabe begonnen werden
+			outMatrix = new JTextArea("Hier hinein kommt Deine Ausgabe als String, Markus");
+			outMatrix.setBounds(410, 30, 350, 120);
+			outMatrix.setVisible(true);
+			this.panel.add(outMatrix);
+			
+			JLabel residuumLabel = new JLabel("Das Residuum:");
+			size = residuumLabel.getPreferredSize();
+			residuumLabel.setBounds(410, 160,size.width,size.height);
+			this.panel.add(residuumLabel);
+			outResiduum = new JTextArea("String Residuum");
+			outResiduum.setBounds(410, 180, 350, 120);
+			outResiduum.setVisible(true);
+			this.panel.add(outResiduum);
+			
+			JLabel eLabel = new JLabel("E r ist:");
+			size = eLabel.getPreferredSize();
+			eLabel.setBounds(410,310,size.width,size.height);
+			this.panel.add(eLabel);
+			oute = new JTextField("Hier der e String");
+			oute.setBounds(410,330, 350, 30);
+			oute.setVisible(true);
+			this.panel.add(oute);
+			
+			JLabel aLabel = new JLabel("||a|| ist:");
+			size = aLabel.getPreferredSize();
+			aLabel.setBounds(410,380, size.width,size.height);
+			this.panel.add(aLabel);
+			oute = new JTextField("Hier der a String");
+			oute.setBounds(410, 400, 350, 30);
+			oute.setVisible(true);
+			this.panel.add(oute);
+
 			this.panel.repaint();
 		}
 		
@@ -126,8 +168,14 @@ public class GUI extends JFrame implements ActionListener{
 			JFileChooser fc = new JFileChooser();
 			fc.showOpenDialog(null);
 		}
+		if (e.getSource()==this.saveItem){
+			//SPEICHERN FUNKTION
+		}
 		if(e.getSource()==this.closeItem){
 			System.exit(0);
+		}
+		if(e.getSource()==this.helpItem){
+			JOptionPane.showMessageDialog(null, "Hier kommt Ilja seine Beschreibung rein");
 		}
 		
 		
