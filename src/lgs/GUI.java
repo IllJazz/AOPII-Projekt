5e628aab6156c297b397d4b49b28ac239f692c53
Markus Müller <markusmueller97@hotmail.de>
2016-07-24 18:08:24 +0200

    gui update x

--- src/lgs/GUI.java
+++ src/lgs/GUI.java
@@ -164,7 +164,6 @@ public class GUI extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
 		if(e.getSource()==this.calcButton){
 			
