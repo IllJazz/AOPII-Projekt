d104449e0095bb53a27c3f9d2a5c8af699779e44
Markus Müller <markusmueller97@hotmail.de>
2016-07-24 18:26:14 +0200

    kleine Fehlerkorrekturen

--- src/lgs/FileIO.java
+++ src/lgs/FileIO.java
@@ -7,9 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Klasse zu Einlesen, Speichern und Verarbeiten von Dateien und Strings 
